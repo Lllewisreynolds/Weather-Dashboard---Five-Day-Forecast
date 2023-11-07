07eec0384364a98c42deaca1783002fdca0017b8
Lllewisreynolds <lsreynolds108@gmail.com>
2023-11-07 12:06:34 +0000

    Functions for Fetch API call requests created. First receives the co-ordinates for the city inputted based on name. Second fetch passes in that lat/lon fata to retrieve five day forecast. Next step is to add Event Listener to button and call this function based on user's selected city input

---
+++ Assets/script.js
@@ -0,0 +1,35 @@
+// API key saved to variable for ease of access
+
+var key = "7db9885d70f5bc0e42c208559c7c142";
+
+// addeventlistener (on) - jquery - button
+
+
+
+function getCoOrdinates(city) {
+    console.log(city);
+    var fetchGeoAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${key}`;
+    fetch(fetchGeoAPI)
+    .then(function(response){
+    console.log(response);
+    return response.json();
+    })
+    .then(function(data){
+    console.log(data);
+    getForecast(data[0].lat, data[0].lon);
+    })
+}
+
+function getForecast(lat, lon) {
+    var fetchAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+    fetch(fetchAPI)
+    .then(function(response){
+    console.log(response);
+    return response.json();
+    })
+    .then(function(data){
+    console.log(data);
+    })
+}
+
+getCoOrdinates("Paris");
