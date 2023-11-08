fd9569e9d365e08701e085e4ab9c2a7fe497d4b1
Lllewisreynolds <lsreynolds108@gmail.com>
2023-11-08 15:21:23 +0000

    Added some trial code in continued attempt to debug calling of event listener on button

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,7 @@ var key = "7db9885d70f5bc0e42c208559c7c142a";
 // jQuery function used to retrieve necessary HTMl elements via their id selector
 
 var cityLocEl = $('#cityloc-input');
+// var city = cityLocEl.val().trim(); - could this be passed in as a parameter?
 var searchBtnEl = $('#search-button');
 var searchedCitiesEl = $('#searched-cities');
 
@@ -38,3 +39,12 @@ function getForecast(lat, lon) {
 
 // temporary function passed - will incorporate a more all-encompassing one later as smaller tasks/functions are still being created
 searchBtnEl.on("click", getCoOrdinates);
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
